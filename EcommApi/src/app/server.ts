4326dd00b9227a75529e780770b15e5f569232a9
ersaumya <ersaumyarout@gmail.com>
2020-03-25 14:25:34 +0530

    added category api

--- EcommApi/src/app/server.ts
+++ EcommApi/src/app/server.ts
@@ -1,3 +1,4 @@
+import { categoryRoute } from './routes/category-routes';
 import { userRoute } from './routes/user-routes';
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
@@ -8,7 +9,10 @@ var app = express();
 //app.get("/", (req, res) => res.send("This is get express Api"));
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+
 app.use("/user",userRoute);
+app.use("/category", categoryRoute);
+
 app.listen(3000, () => {
     MongoConnect.connect().then((res)=>console.log("DB Connected"));
     console.log("Server running on port 3000")
