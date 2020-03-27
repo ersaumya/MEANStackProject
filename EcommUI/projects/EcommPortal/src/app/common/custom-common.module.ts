ac909e5755b7865fb8ce2e6e8ceeff5882dc9b68
ersaumya <ersaumyarout@gmail.com>
2020-03-27 15:04:05 +0530

    change folder structure UI

--- EcommUI/projects/EcommPortal/src/app/common/custom-common.module.ts
+++ EcommUI/projects/EcommPortal/src/app/common/custom-common.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonRoutingModule } from './common-routing.module';
-import { LoginComponent } from './login/login.component';
-import { MainNavComponent } from './main-nav/main-nav.component';
+import { LoginComponent } from './components/login/login.component';
+import { MainNavComponent } from './components/main-nav/main-nav.component';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '../shared/material.module';
 
