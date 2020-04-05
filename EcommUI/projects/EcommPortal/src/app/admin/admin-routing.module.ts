ecc6fa063c6909cb8ae622cc2d3d3e51c15c6ede
ersaumya <ersaumyarout@gmail.com>
2020-04-05 17:41:45 +0530

    admin & user guard added

--- EcommUI/projects/EcommPortal/src/app/admin/admin-routing.module.ts
+++ EcommUI/projects/EcommPortal/src/app/admin/admin-routing.module.ts
@@ -2,10 +2,15 @@ import { AuthGuard } from './../common/guard/auth.guard';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+import { AdminGuard } from './guard/admin.guard';
 
 
 const routes: Routes = [
-  { path: "admin/dashboard", component: AdminDashboardComponent ,canActivate:[AuthGuard]}
+  {
+    path: "admin/dashboard",
+    component: AdminDashboardComponent,
+    canActivate: [AuthGuard, AdminGuard]
+  }
 ];
 
 @NgModule({
--- EcommUI/projects/EcommPortal/src/app/user/user-routing.module.ts
+++ EcommUI/projects/EcommPortal/src/app/user/user-routing.module.ts
@@ -2,10 +2,15 @@ import { AuthGuard } from './../common/guard/auth.guard';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserDashboardComponent } from './components/user-dashboard/user-dashboard.component';
+import { UserGuard } from './guard/user.guard';
 
 
 const routes: Routes = [
-  { path: "user/dashboard", component: UserDashboardComponent ,canActivate:[AuthGuard]}
+  {
+    path: "user/dashboard",
+    component: UserDashboardComponent,
+    canActivate: [AuthGuard, UserGuard]
+  }
 ];
 
 @NgModule({
